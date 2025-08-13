76dcefc55ae9a67589141be3f9dc728991db54ac
jmho <jaylenho734@gmail.com>
2025-08-13 15:48:00 -0400

    chore: initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+
+const app = new Elysia()
+  .use(swagger())
+  .get(
+    "/user/:id",
+    ({ params: { id } }) => {
+      console.log("test");
+    },
+    {
+      params: t.Object({
+        id: t.Number(),
+      }),
+    }
+  )
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
+
+export type App = typeof app;
